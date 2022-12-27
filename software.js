b8d613a4d08d4cedc704cb5da6de833dbb0e0480
Shyam_sundar <120415252+shaaamsundar@users.noreply.github.com>
2022-12-27 16:15:08 +0530

    Add files via upload

---
+++ software.js
@@ -0,0 +1,29 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0%";
+}
+
+var currentIndex = 1;
+displaySlides(currentIndex);
+
+function setSlides(num) {
+    displaySlides(currentIndex += num);
+}
+function displaySlides(num) {
+    var x;
+    var slides = document.getElementsByClassName("imageSlides");
+    if (num > slides.length) {
+        currentIndex = 1
+    }
+    if (num < 1) {
+        currentIndex = slides.length
+    }
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+    }
+    slides[currentIndex - 1].style.display = "block";
+}
+
